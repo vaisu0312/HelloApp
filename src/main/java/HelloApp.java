94bdc7fb04878351a4de8c27ff7457df257bff5d
vaisu0312 <vaishnavikashyap.pratt@gmail.com>
2026-03-22 12:07:20 +0530

    UC4 completed

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name = "World"; // default value
+        String name;
 
         if (args.length > 0) {
-            name = args[0];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+                if (i < args.length - 1) {
+                    sb.append(", ");
+                }
+            }
+
+            name = sb.toString();
+        } else {
+            name = "World";
         }
 
         System.out.println("Hello, " + name + "!");
